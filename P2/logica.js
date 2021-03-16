2ad42fc297d9b7116611e582dcbf34f25e7ca8bf
jgozalo <javinl7@hotmail.com>
2021-03-16 10:48:58 +0100

    onClick de 6 botones con "this"

--- P2/logica.js
+++ P2/logica.js
@@ -3,14 +3,15 @@ console.log("Consola")
 
 function operaciones(a)
 {
-    document.getElementById('result').innerHTML = "hola"
+    document.getElementById('result').innerHTML = this.innerHTML
 }
 
 
 var result = document.getElementById('result')
 
-
-
 document.getElementById('num1').addEventListener("click", operaciones);
 document.getElementById('num2').addEventListener("click", operaciones);
 document.getElementById('num3').addEventListener("click", operaciones);
+document.getElementById('num4').addEventListener("click", operaciones);
+document.getElementById('num5').addEventListener("click", operaciones);
+document.getElementById('num6').addEventListener("click", operaciones);
