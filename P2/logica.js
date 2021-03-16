29adbc1ed9bd3dce2965a8b211a252b38687f31b
jgozalo <javinl7@hotmail.com>
2021-03-16 10:07:44 +0100

    Funcion onClick

--- P2/logica.js
+++ P2/logica.js
@@ -1,5 +1,14 @@
 //Lógica de calculadora
 console.log("Consola")
 
-const num = document.getElementById('num')
-console.log(num.innerHTML)
+
+
+const num1 = document.getElementById('num1')
+const num2 = document.getElementById('num2')
+const num3 = document.getElementById('num3')
+
+var result = document.getElementById('result')
+console.log(num1.innerHTML + num2.innerHTML + num3.innerHTML)
+num1.onclick = () => {
+    result.innerHTML = "1"
+}
