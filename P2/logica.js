c9644ca48f10fe4b679004266fc24d5d14801ab2
jgozalo <javinl7@hotmail.com>
2021-03-16 15:24:36 +0100

    Cambio en botones

--- P2/logica.js
+++ P2/logica.js
@@ -3,12 +3,36 @@
 
 console.log("Consola")
 
-function operaciones(a)
+function botonPulsado(a)
 {
-    document.getElementById('result').innerHTML = this.innerHTML
+    //document.getElementById('result').innerHTML = this.innerHTML
+    console.log(this.innerHTML)
 }
 
 
 var result = document.getElementById('result')
 
-document.getElementById('bu1').addEventListener("click", operaciones);
+document.getElementById('buac').addEventListener("click", botonPulsado);
+document.getElementById('buper').addEventListener("click", botonPulsado);
+document.getElementById('buel').addEventListener("click", botonPulsado);
+document.getElementById('buc').addEventListener("click", botonPulsado);
+
+document.getElementById('bu7').addEventListener("click", botonPulsado);
+document.getElementById('bu8').addEventListener("click", botonPulsado);
+document.getElementById('bu9').addEventListener("click", botonPulsado);
+document.getElementById('budiv').addEventListener("click", botonPulsado);
+
+document.getElementById('bu4').addEventListener("click", botonPulsado);
+document.getElementById('bu5').addEventListener("click", botonPulsado);
+document.getElementById('bu6').addEventListener("click", botonPulsado);
+document.getElementById('bumul').addEventListener("click", botonPulsado);
+
+document.getElementById('bu1').addEventListener("click", botonPulsado);
+document.getElementById('bu2').addEventListener("click", botonPulsado);
+document.getElementById('bu3').addEventListener("click", botonPulsado);
+document.getElementById('bumas').addEventListener("click", botonPulsado);
+
+document.getElementById('bu0').addEventListener("click", botonPulsado);
+document.getElementById('bupoi').addEventListener("click", botonPulsado);
+document.getElementById('bueq').addEventListener("click", botonPulsado);
+document.getElementById('bumen').addEventListener("click", botonPulsado);
