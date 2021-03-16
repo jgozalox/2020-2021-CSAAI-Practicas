448e6e434bb7b27bdf8e4406c94645836e13fc79
jgozalo <javinl7@hotmail.com>
2021-03-17 00:29:20 +0100

    Cambio de camino para resolver la logica de la calculadora

--- P2/logica.js
+++ P2/logica.js
@@ -2,6 +2,8 @@
 //Formato expresiones: <numero> <operador> <numero> (solo 2 numeros, no ampliable a 3)
 
 console.log("Consola")
+
+var tresElem = [];
 var primerNum;
 var operando;
 var segundoNum;
@@ -10,39 +12,33 @@ function botonPulsado(a)
 {
     //document.getElementById('result').innerHTML = this.innerHTML
     console.log(this.innerHTML)
-    if (result.innerHTML != "Resultado"){
-            result.innerHTML += this.innerHTML
-    } else{
-        result.innerHTML = this.innerHTML
-    }
-    
+    result.innerHTML += this.innerHTML
+
 
 }
 
 
 var result = document.getElementById('result')
 
+
+var num1 = document.getElementById('bu1')
+var num2 = document.getElementById('bu2')
+var num3 = document.getElementById('bu3')
+var num4 = document.getElementById('bu4')
+var num5 = document.getElementById('bu5')
+var num6 = document.getElementById('bu6')
+var num7 = document.getElementById('bu7')
+var num8 = document.getElementById('bu8')
+var num9 = document.getElementById('bu9')
+var botonesNumero = [num1, num2, num3, num4, num5, num6, num7, num8, num9];
+
 document.getElementById('buac').addEventListener("click", botonPulsado);
 document.getElementById('buper').addEventListener("click", botonPulsado);
 document.getElementById('buel').addEventListener("click", botonPulsado);
 document.getElementById('buc').addEventListener("click", botonPulsado);
-
-document.getElementById('bu7').addEventListener("click", botonPulsado);
-document.getElementById('bu8').addEventListener("click", botonPulsado);
-document.getElementById('bu9').addEventListener("click", botonPulsado);
 document.getElementById('budiv').addEventListener("click", botonPulsado);
-
-document.getElementById('bu4').addEventListener("click", botonPulsado);
-document.getElementById('bu5').addEventListener("click", botonPulsado);
-document.getElementById('bu6').addEventListener("click", botonPulsado);
 document.getElementById('bumul').addEventListener("click", botonPulsado);
-
-document.getElementById('bu1').addEventListener("click", botonPulsado);
-document.getElementById('bu2').addEventListener("click", botonPulsado);
-document.getElementById('bu3').addEventListener("click", botonPulsado);
 document.getElementById('bumas').addEventListener("click", botonPulsado);
-
-document.getElementById('bu0').addEventListener("click", botonPulsado);
 document.getElementById('bupoi').addEventListener("click", botonPulsado);
 document.getElementById('bueq').addEventListener("click", botonPulsado);
 document.getElementById('bumen').addEventListener("click", botonPulsado);
