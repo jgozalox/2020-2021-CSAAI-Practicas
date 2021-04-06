07d9f1ef8b434b51e7fc662a82ba86076360129e
jgozalo <javinl7@hotmail.com>
2021-04-06 09:57:30 +0200

    Asignacion de JS correcto

--- P2/logica.js
+++ P2/logica.js
@@ -11,22 +11,21 @@ var resultadoOp;
 var result = document.getElementById('result')
 var banderaOnOff = false;
 
-function botonPulsado(a)
-{
-    if (this.innerHTML == "AC"){
+function botonPulsado(a){
+    if (a.target.value == "AC"){
       tresElem = [];
       result.innerHTML = " "
-    }else if(this.innerHTML == "DEL"){
+    }else if(a.target.value == "DEL"){
       console.log("borrar")
-    }else if (isNaN(this.innerHTML) && this.innerHTML != "." ){
+    }else if (isNaN(a.target.value) && a.target.value != "." ){
       //Pulsacion de operador
       if (tresElem.length == 0){
         //No hará nada ya que no hay num (array vacio)
         console.log("Introduzca primero un numero")
       }else{
-        if (this.innerHTML != "="){
+        if (a.target.value != "="){
           if(tresElem.length == 1){
-            tresElem[1] = this.innerHTML;
+            tresElem[1] = a.target.value;
             result.innerHTML += tresElem[1];
             console.log("elem2",tresElem[1])
           }
@@ -65,35 +64,33 @@ function botonPulsado(a)
       //Pulsacion de numero
       if(tresElem.length < 2){
         if (tresElem.length == 0){
-          tresElem[0] = this.innerHTML
-          result.innerHTML += this.innerHTML
+          tresElem[0] = a.target.value
+          result.innerHTML += a.target.value
         }else{
-          if (tresElem[0].includes(".") == true && this.innerHTML == "."){
+          if (tresElem[0].includes(".") == true && a.target.value == "."){
             console.log("Ya hay un punto")
           }else{
-            tresElem[0] += this.innerHTML
-            result.innerHTML += this.innerHTML
+            tresElem[0] += a.target.value
+            result.innerHTML += a.target.value
           }
         }
         console.log("elem1",tresElem[0])
       }else if(tresElem.length >= 2){
         if (tresElem.length == 2){
-          tresElem[2] = this.innerHTML
-          result.innerHTML += this.innerHTML
+          tresElem[2] = a.target.value
+          result.innerHTML += a.target.value
         }else{
-          if (tresElem[2].includes(".") == true && this.innerHTML == "."){
+          if (tresElem[2].includes(".") == true && a.target.value == "."){
             console.log("Ya hay un punto")
           }else{
-            tresElem[2] += this.innerHTML
-            result.innerHTML += this.innerHTML
+            tresElem[2] += a.target.value
+            result.innerHTML += a.target.value
           }
         }
         
         console.log("elem3",tresElem[2])
       }
     }
-
-
 }
 
 function encender(){
@@ -108,51 +105,11 @@ function encender(){
   result.innerHTML = " "
 }
 
-
 document.getElementById('onoffbutton').onclick = function(){encender()};
+botones = document.getElementsByClassName("boton")
 
-
-
-const gui = {
-  //Operandos
-  ac:  document.getElementById('buac'),
-  per:document.getElementById('buper'),
-  el: document.getElementById('buel'),
-  c: document.getElementById('budel'),
-  div: document.getElementById('budiv'),
-  mul : document.getElementById('bumul'),
-  mas : document.getElementById('bumas'),
-  poi : document.getElementById('bupoi'),
-  eq : document.getElementById('bueq'),
-  men : document.getElementById('bumen'),
-  //Numeros
-  num0 : document.getElementById('bu0'),
-  num1 : document.getElementById('bu1'),
-  num2 : document.getElementById('bu2'),
-  num3 : document.getElementById('bu3'),
-  num4 : document.getElementById('bu4'),
-  num5 : document.getElementById('bu5'),
-  num6 : document.getElementById('bu6'),
-  num7 : document.getElementById('bu7'),
-  num8 : document.getElementById('bu8'),
-  num9 : document.getElementById('bu9')
-
-  
+for (let boton of botones) {
+    boton.onclick = (ev) => {
+        botonPulsado(ev)
+    }
 }
-const botonesNumero = [gui.num0, gui.num1, gui.num2, gui.num3, gui.num4, gui.num5, gui.num6, gui.num7, gui.num8, gui.num9];
-const botonesOperando = [gui.ac, gui.per, gui.el, gui.c, gui.div, gui.mul, gui.mas, gui.poi, gui.eq, gui.men]
-
-for (var i=0; i<botonesOperando.length; i++){ 
-  //console.log(botonesOperando[i].innerHTML,typeof(botonesOperando[i].innerHTML));
-  botonesOperando[i].addEventListener("click", botonPulsado);
-}
-
-for (var i=0; i<botonesNumero.length; i++){ 
-  //console.log(botonesNumero[i].innerHTML,typeof(botonesNumero[i].innerHTML));
-  botonesNumero[i].addEventListener("click", botonPulsado);
-}
-
-//Otra forma de hacer el bucle for
-//nombreArray.forEach(function(elemento,indice,array){
-//    console.log(elemento.innerHTML, indice)
-//})
