ada9b9336e96e15a061d5f553f3855d0f8580832
jgozalo <javinl7@hotmail.com>
2021-04-06 10:33:51 +0200

    Animacion

--- P3/logica.js
+++ P3/logica.js
@@ -2,26 +2,49 @@ console.log("Ejecutando JS...");
 
 const canvas = document.getElementById("canvas");
 
-//-- Definir el tamaño del convas
-canvas.width = 600;
+//-- Definir el tamaño del canvas
+canvas.width = 300;
 canvas.height = 100;
 
 //-- Obtener el contexto del canvas
 const ctx = canvas.getContext("2d");
 
-//-- Cada objeto a dibujar lo delimitaremos 
-//-- por los métodos beginPath() y closePath()
-ctx.beginPath();
-  //-- Definir un rectangulo de dimensiones 100x50,
-  //-- cuya esquina superior izquierda está en (5,5)
-  ctx.rect(5,5, 100, 50);
+//-- Coordenadas del objeto
+let x = 0;
+let y = 10;
 
-  //-- Color de relleno del rectángulo
-  ctx.fillStyle = 'blue';
+//-- Velocidad horizontal del objeto
+let velx = 1;
 
-  //-- Mostrar el relleno
-  ctx.fill();
+//-- Funcion principal de animacion
+function update() 
+{
+  console.log("test");
+  //-- Algoritmo de animacion:
+  //-- 1) Actualizar posicion del  elemento
+  //-- (física del movimiento rectilineo uniforme)
+  x = x + velx;
 
-  //-- Mostrar el trazo del rectángulo
-  ctx.stroke();
-ctx.closePath();
+  //-- 2) Borrar el canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  //-- 3) Dibujar los elementos visibles
+  ctx.beginPath();
+    ctx.rect(x, y, 20, 20);
+
+    //-- Dibujar
+    ctx.fillStyle = 'red';
+
+    //-- Rellenar
+    ctx.fill();
+
+    //-- Dibujar el trazo
+    ctx.stroke()
+  ctx.closePath();
+
+  //-- 4) Volver a ejecutar update cuando toque
+  requestAnimationFrame(update);
+}
+
+//-- ¡Que empiece la función!
+update();
