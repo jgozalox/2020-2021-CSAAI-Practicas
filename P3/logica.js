bada799596073abefcf9136791bff8e7bacedf7d
jgozalo <javinl7@hotmail.com>
2021-04-12 22:53:23 +0200

    Problema de velocidad aparentemente arreglado

--- P3/logica.js
+++ P3/logica.js
@@ -4,7 +4,7 @@ const canvas = document.getElementById("canvas");
 
 //-- Definir el tamaño del canvas
 canvas.width = 300;
-canvas.height = 100;
+canvas.height = 400;
 
 //-- Obtener el contexto del canvas
 const ctx = canvas.getContext("2d");
@@ -14,47 +14,6 @@ let y = 0;
 
 let velx = 6;
 let vely = 2;
-
-let enJuego = false;
-
-function movimiento() 
-{
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.beginPath();
-    ctx.rect(x, y, 10, 10);
-    ctx.fillStyle = 'white';
-    ctx.fill();
-    ctx.stroke()
-  ctx.closePath();
-
-  if (enJuego == true){
-    x = x + velx;
-    y = y + vely;
-
-    if (x < 0 || x >= (canvas.width - 10) ) {
-      velx = -velx;
-    }
-  
-    if (y < 0) {
-      vely = -vely;
-    }
-
-    if(y >= (canvas.height - 10)){
-      x = 0;
-      y = 0;
-      velx = 6;
-      vely = 2;
-      document.getElementById("start").innerHTML = "START";
-      enJuego = false;
-    }
-
-  }
-
-  //requestAnimationFrame(movimiento);
-  document.getElementById("start").addEventListener("click", movimiento);
-  console.log("vel x = ",velx," vel y=",vely)
-}
 
 function dibujo() 
 {
@@ -68,10 +27,49 @@ function dibujo()
 }
 dibujo();
 
-document.getElementById("start").addEventListener("click", myFunction);
+function movimiento() 
+{
+
+  if (x < 0 || x >= (canvas.width - 10) ) {
+    velx = -velx;
+  }
+    
+  if (y < 0) {
+    vely = -vely;
+  }
+
+  if(y >= (canvas.height - 10)){
+    x = 0;
+    y = 0;
+    dibujo();
+    return;
+  }  
+
+    
+  x = x + velx;
+  y = y + vely;
+
+
+  requestAnimationFrame(movimiento);
+  
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+    ctx.rect(x, y, 10, 10);
+    ctx.fillStyle = 'white';
+    ctx.fill();
+    ctx.stroke()
+  ctx.closePath();
+  
+  
+}
 
 function myFunction() {
   document.getElementById("start").innerHTML = "EN MARCHA!";
-  enJuego = true;
   movimiento();
 }
+
+
+
+
+document.getElementById("start").addEventListener("click", myFunction);
+
