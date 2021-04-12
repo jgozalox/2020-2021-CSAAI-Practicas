fb1cc20e14a86047238ca2f20b2a36536059e413
jgozalo <javinl7@hotmail.com>
2021-04-12 19:29:00 +0200

    Cambios y boton Start más funcional

--- P3/logica.js
+++ P3/logica.js
@@ -18,13 +18,13 @@ let velx = 3;
 let vely = 1;
 
 //-- Funcion principal de animacion
-function updateSquare() 
+function movimiento() 
 {
    if (x < 0 || x >= (canvas.width - 10) ) {
     velx = -velx;
   }
 
-  if (y < 0 || y > (canvas.height - 10)) {
+  if (y < 0) {
     vely = -vely;
   }
 
@@ -38,26 +38,27 @@ function updateSquare()
     ctx.stroke()
   ctx.closePath();
 
-  //-- Actualizar la posición
   x = x + velx;
   y = y + vely;
 
-  //-- 4) Volver a ejecutar updateSquare cuando toque
-  requestAnimationFrame(updateSquare);
+  requestAnimationFrame(movimiento);
 }
 
-let step = 20
-
-
-//document.addEventListener("keydown", myFunction);
-
-//function myFunction() {
-  //console.log("prueba")
-//}
+function dibujo() 
+{
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+    ctx.rect(x, y, 10, 10);
+    ctx.fillStyle = 'white';
+    ctx.fill();
+    ctx.stroke()
+  ctx.closePath();
+}
+dibujo();
 
 document.getElementById("start").addEventListener("click", myFunction);
 
 function myFunction() {
   document.getElementById("start").innerHTML = "EN MARCHA!";
-  updateSquare();
+  movimiento();
 }
