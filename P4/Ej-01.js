3cec9e4e678b8458300b7a3acb1ae92a47c4557c
jgozalo <javinl7@hotmail.com>
2021-05-20 00:41:33 +0200

    Umbrales y componentes

--- P4/Ej-01.js
+++ P4/Ej-01.js
@@ -7,9 +7,13 @@ const ctx = canvas.getContext('2d');
 
 //-- Acceso al deslizador
 const deslizadorR = document.getElementById('deslizadorR');
+const deslizadorG = document.getElementById('deslizadorG');
+const deslizadorB = document.getElementById('deslizadorB');
 
 //-- Valor del deslizador
 const range_valueR = document.getElementById('range_valueR');
+const range_valueG = document.getElementById('range_valueG');
+const range_valueB = document.getElementById('range_valueB');
 
 //-- Función de retrollamada de imagen cargada
 //-- La imagen no se carga instantaneamente, sino que
@@ -29,6 +33,23 @@ img.onload = function () {
   console.log("Imagen lista...");
 };
 
+function compontentes(){
+  var umbralR = deslizadorR.value
+  var umbralG = deslizadorG.value
+  var umbralB = deslizadorB.value
+
+  for (var i = 0; i < data.length; i+=4) {
+    if (data[i] > umbralR){
+      data[i] = umbralR;
+    }
+    if (data[i+1] > umbralG){
+      data[i+1] = umbralG;
+    }
+    if (data[i+2] > umbralB){
+      data[i+2] = umbralB;
+    }
+  }
+}
 
 //-- Funcion de retrollamada del deslizador
 deslizadorR.oninput = () => {
@@ -45,14 +66,47 @@ deslizadorR.oninput = () => {
   //-- Obtener el array con todos los píxeles
   let data = imgData.data
 
-  //-- Obtener el umbral de rojo del desliador
-  umbral = deslizadorR.value
+  compontentes();
 
-  //-- Filtrar la imagen según el nuevo umbral
-  for (let i = 0; i < data.length; i+=4) {
-    if (data[i] > umbral)
-      data[i] = umbral;
-  }
+  //-- Poner la imagen modificada en el canvas
+  ctx.putImageData(imgData, 0, 0);
+}
+
+deslizadorG.oninput = () => {
+  //-- Mostrar el nuevo valor del deslizador
+  range_valueG.innerHTML = deslizadorG.value;
+
+  //-- Situar la imagen original en el canvas
+  //-- No se han hecho manipulaciones todavia
+  ctx.drawImage(img, 0,0);
+
+  //-- Obtener la imagen del canvas en pixeles
+  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  //-- Obtener el array con todos los píxeles
+  let data = imgData.data
+
+  compontentes();
+
+  //-- Poner la imagen modificada en el canvas
+  ctx.putImageData(imgData, 0, 0);
+}
+
+deslizadorB.oninput = () => {
+  //-- Mostrar el nuevo valor del deslizador
+  range_valueB.innerHTML = deslizadorB.value;
+
+  //-- Situar la imagen original en el canvas
+  //-- No se han hecho manipulaciones todavia
+  ctx.drawImage(img, 0,0);
+
+  //-- Obtener la imagen del canvas en pixeles
+  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  //-- Obtener el array con todos los píxeles
+  let data = imgData.data
+  
+  compontentes();
 
   //-- Poner la imagen modificada en el canvas
   ctx.putImageData(imgData, 0, 0);
